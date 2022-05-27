b1afca3a4db16fd5310169ea529afadcf9725cae
Seve999 <xxspwiki@gmail.com>
2022-05-27 16:09:16 +0200

    layout dettaglio resort

--- app/src/main/java/it/pietrantuono/skyitaly/ui/LoginActivity.java
+++ app/src/main/java/it/pietrantuono/skyitaly/ui/LoginActivity.java
@@ -1,19 +1,67 @@
 package it.pietrantuono.skyitaly.ui;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import it.pietrantuono.skyitaly.R;
 import it.pietrantuono.skyitaly.databinding.ActivityLoginBinding;
 
 public class LoginActivity extends BaseActivity {
 
     private ActivityLoginBinding binding;
+    private String txtemail;
+    private String txtPassword;
+
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding = ActivityLoginBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
+        binding = ActivityLoginBinding.inflate(getLayoutInflater());   // accesso a tute le risorse grafiche inserite nel activity log_in xml
+        View view= binding.getRoot();
+        setContentView(view);     //
+
+         //leggere ciò che è stato scritto nel campo password
+        binding.btnLogin.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v){
+                txtemail=binding.email.getText().toString();       //leggere ciò che è stato scritto nel campo email
+                txtPassword=binding.password.getText().toString();
+                if(checkField(txtemail,txtPassword) && checkEmail(txtemail) && checkPassword(txtPassword))
+                    Toast.makeText(v.getContext(),"CampiPieni",Toast.LENGTH_LONG).show();
+                else
+                    Toast.makeText(v.getContext(),"CampiErrati",Toast.LENGTH_LONG).show();
+
+            }
+        });
     }
+    private boolean checkField(String username,String password) {     // da implementare(controllo due campi email e password)
+        return !(username.isEmpty() || password.isEmpty());
+
+    }
+
+    public boolean checkEmail(String email) {
+        String regex ="^\\w+([-+.']\\w+)*@\\w+([-.]\\w+)*\\.\\w+([-.]\\w+)*$";         // ^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(email);
+        Boolean result = matcher.matches();
+                return result;
+    }
+
+    public boolean checkPassword(String password){
+        if(password.length()<6 ) return false;
+        return true;
+    }
+
+
+
+
 }
+
+
---
+++ app/src/main/java/it/pietrantuono/skyitaly/ui/RecuperoPassword.java
@@ -0,0 +1,30 @@
+package it.pietrantuono.skyitaly.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+import it.pietrantuono.skyitaly.databinding.ActivityLoginBinding;
+import it.pietrantuono.skyitaly.databinding.ActivityRecuperoPasswordBinding;
+
+public class RecuperoPassword extends AppCompatActivity {
+
+    private ActivityRecuperoPasswordBinding binding;
+    private String txtemail;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding =ActivityRecuperoPasswordBinding.inflate(getLayoutInflater());
+        View view= binding.getRoot();
+        setContentView(view);
+
+        binding.btnResetPassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                txtemail=binding.email.getText().toString();
+            }
+        });
+    }
+}
